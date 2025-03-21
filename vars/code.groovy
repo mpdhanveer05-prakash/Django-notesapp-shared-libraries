934801c008acac272f9127053157adeeef1cf717
M.P.DHANVEER PRAKASH <94118599+mpdhanveer05-prakash@users.noreply.github.com>
2025-03-21 22:43:34 +0530

    Updated code.groovy

--- vars/code.groovy
+++ vars/code.groovy
@@ -1,4 +1,4 @@
-def call(String url, string branch){
+def call(String url, String branch){
   echo "This is Cloning the code"
   git url: "${url}", branch: "${branch}"
   echo "Code cloning successful"
