4f36abdb2f2494c03fc2f9a7fcbce5b4e7d7beb6
M.P.DHANVEER PRAKASH <94118599+mpdhanveer05-prakash@users.noreply.github.com>
2025-03-21 22:17:48 +0530

    Delete vars/vars directory

--- vars/vars/push.groovy
+++
@@ -1,7 +0,0 @@
-def call(String ProjectName, String ImageTag, String dockerhubuser){
-  echo "This is Pushing the image to docker hub"
-  withCredentials([usernamePassword('credentialsId':"dockerhubcred", passwordVariable:"dockerhubpass", usernameVariable:"dockerhubuser")]){
-    sh "docker login -u ${env.dockerhubuser} -p ${env.dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${ProjectName}:${ImageTag}"
-}
